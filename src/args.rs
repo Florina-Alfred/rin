7117028bd2b3d667be4e29a01b6240dc2a0f0fd5
Florina-Alfred <sonofcop@gmail.com>
2025-01-07 20:24:22 +0100

    added help messages for the args

--- src/args.rs
+++ src/args.rs
@@ -3,19 +3,24 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Args {
+    /// Start value for the stream
     #[arg(short, long, default_value_t = 0)]
     pub start: u32,
 
+    /// Key expression for the publisher
     #[arg(short, long, default_value = "test_topic")]
     pub input_key_expr: String,
 
+    /// Key expression for the subscriber
     #[arg(short, long, default_value = "test_topic")]
     pub output_key_expr: String,
 
+    /// Mode of operation
     #[arg(short, long, default_value = "client")]
     // #[arg(short, long, default_value = "peer")]
     pub mode: String,
 
+    /// Endpoints for the publisher/subscriber
     #[arg(short, long, default_value = "tcp/0.0.0.0:7447")]
     // #[arg(short, long, default_value = "")]
     pub endpoints: Vec<String>,
