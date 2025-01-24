e82a7ca41d5bf39b54eefd58c9760fc859ade89b
Florina-Alfred <sonofcop@gmail.com>
2025-01-24 15:40:04 +0100

    testing out spawn tasks for channel based communication in ws

--- src/temp.rs
+++ src/temp.rs
@@ -1,46 +1,93 @@
-use serde_json::json;
-use zenoh::sample::SampleKind;
-use zenoh::Config;
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::sync::Mutex;
+use tokio::sync::broadcast;
+use tokio::sync::broadcast::{Receiver, Sender};
 
 #[tokio::main]
 async fn main() {
-    // initiate logging
+    // let mut topic_channel: HashMap<String, Vec<String>> = HashMap::new();
+    let mut topic_channel: Arc<Mutex<HashMap<String, Vec<String>>>> =
+        Arc::new(Mutex::new(HashMap::new()));
+    let topics = vec!["topic1", "topic2", "topic3"]
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<String>();
 
-    let mut config = Config::default();
-    config
-        .insert_json5("mode", &json!("peer").to_string())
-        .unwrap();
-    let _ = config.insert_json5(
-        "connect/endpoints",
-        &json!(["tcp/0.0.0.0:7447"]).to_string(),
-    );
+    let topic1_msgs = vec![
+        "topic1_msg1",
+        "topic1_msg2",
+        "topic1_msg3",
+        "topic1_msg4",
+        "topic1_msg5",
+        "topic1_msg6",
+        "topic1_msg7",
+        "topic1_msg8",
+        "topic1_msg9",
+        "topic1_msg10",
+    ]
+    .iter()
+    .map(|x| x.to_string())
+    .collect::<Vec<String>>();
+    let topic2_msgs = vec!["topic2_msg1", "topic2_msg2"]
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>();
+    let topic3_msgs = vec![
+        "topic3_msg1",
+        "topic3_msg2",
+        "topic3_msg3",
+        "topic3_msg4",
+        "topic3_msg5",
+        "topic3_msg6",
+    ]
+    .iter()
+    .map(|x| x.to_string())
+    .collect::<Vec<String>>();
 
-    println!("Opening session...");
-    let session = zenoh::open(config).await.unwrap();
-
-    let key_expr = "**";
-    let history = true;
-
-    println!("Declaring Liveliness Subscriber on '{}'...", &key_expr);
-
-    let subscriber = session
-        .liveliness()
-        .declare_subscriber(key_expr)
-        .history(history)
-        .await
-        .unwrap();
-
-    println!("Press CTRL-C to quit...");
-    while let Ok(sample) = subscriber.recv_async().await {
-        match sample.kind() {
-            SampleKind::Put => println!(
-                ">> [LivelinessSubscriber] New alive token ('{}')",
-                sample.key_expr().as_str()
-            ),
-            SampleKind::Delete => println!(
-                ">> [LivelinessSubscriber] Dropped token ('{}')",
-                sample.key_expr().as_str()
-            ),
+    let topic_channel_clone = topic_channel.clone();
+    let insert_task = tokio::task::spawn(async move {
+        for i in 0..10 {
+            let mut topic_channel_guard = topic_channel_clone.lock().unwrap();
+            tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+            match topic_channel_guard.get_mut(&format!("topic{}", (i % 3) + 1)) {
+                Some(channels) => {
+                    let (tx, rx): (Sender<String>, Receiver<String>) = broadcast::channel(10);
+                    channels.push(format!("rx{}", i));
+                }
+                None => {
+                    topic_channel_guard
+                        .insert(format!("topic{}", (i % 3) + 1), vec![format!("rx{}", i)]);
+                }
+            }
         }
-    }
+    });
+    println!("topics: {:?}", topic_channel);
+
+    let topic_channel_clone = Arc::clone(&topic_channel);
+    tokio::spawn(async move {
+        for msg in topic1_msgs {
+            println!("topic1: {:?}", msg);
+            println!("topic_clone: {:?}", topic_channel_clone);
+            tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+        }
+    });
+    let topic_channel_clone = Arc::clone(&topic_channel);
+    tokio::spawn(async move {
+        for msg in topic2_msgs {
+            println!("topic2: {:?}", msg);
+            println!("topic_clone: {:?}", topic_channel_clone);
+            tokio::time::sleep(tokio::time::Duration::from_micros(10)).await;
+        }
+    });
+    let topic_channel_clone = Arc::clone(&topic_channel);
+    tokio::spawn(async move {
+        for msg in topic3_msgs {
+            println!("topic3: {:?}", msg);
+            println!("topic_clone: {:?}", topic_channel_clone);
+            tokio::time::sleep(tokio::time::Duration::from_micros(1)).await;
+        }
+    });
+    tokio::time::sleep(tokio::time::Duration::from_secs(15)).await;
+    insert_task.await.unwrap();
 }
