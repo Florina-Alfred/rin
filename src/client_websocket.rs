693ccee559ef09cb73e360c43ac07ee1a3d2eb96
Florina-Alfred <sonofcop@gmail.com>
2025-01-28 01:19:33 +0100

    modified client webscoket to have log messages and print only data

--- src/client_websocket.rs
+++ src/client_websocket.rs
@@ -8,15 +8,15 @@ async fn main() {
     let topic = std::env::args()
         .nth(1)
         .expect("add a topic to subscribe to");
-    let server = format!("ws://127.0.0.1:3212/ws/{}", topic);
+    let server = format!("ws://0.0.0.0:3212/ws/sub/{}", topic);
     let ws_stream = match connect_async(server).await {
         Ok((stream, response)) => {
-            println!("Handshake for client has been completed");
-            println!("Server response was {response:?}");
+            tracing::info!("Handshake for client has been completed");
+            tracing::info!("Server response was {response:?}");
             stream
         }
         Err(e) => {
-            println!("WebSocket handshake for client failed with {e}!");
+            tracing::info!("WebSocket handshake for client failed with {e}!");
             return;
         }
     };
@@ -27,10 +27,11 @@ async fn main() {
         if let Message::Text(txt) = msg {
             let txt = serde_json::from_str::<serde_json::Value>(&txt)
                 .expect("Failed to parse JSON message");
-            println!("Received a text message: {}", txt);
+            tracing::info!("Received a text message: {}", txt);
+            println!("{}", txt);
         }
     }
 
     let time_elapsed = start_time.elapsed();
-    println!("Client finished in {:.3?}", time_elapsed);
+    tracing::info!("Client finished in {:.3?}", time_elapsed);
 }
